e10ba7b7c345ce78afaf4aaf4ce9dff13ff7ef1d
Marko721 <popmark1002@gmail.com>
2021-08-13 18:03:46 +0200

    update

--- src/Components/Guess.js
+++ src/Components/Guess.js
@@ -1,22 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Guess = ({ hiddenWord, word }) => {
   const [letters, setLetters] = useState([]);
+  // const [letter, setLetter] = useEffect("");
   const joinedWord = word.join("");
 
   document.addEventListener("keypress", (e) => {
     if (e.key.match(/[a-z]/i)) {
       let letter = e.key.toUpperCase();
-      console.log(letter);
+      // console.log(letter);
       // pozivanje funckije koja proverava
       checkAndReplace(letter, word);
     }
   });
 
-  const checkAndReplace = (letter, word) => {};
+  const checkAndReplace = (e) => {
+    console.log(e);
+  };
 
   return (
-    <div className="text">
+    <div className="text" onKeyDown={() => checkAndReplace()}>
       <h3 className="guess">{hiddenWord}</h3>
     </div>
   );
