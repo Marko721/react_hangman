44826b2b5eda877c9d7967c0cfcd2e84abb0f27d
Marko721 <popmark1002@gmail.com>
2021-08-28 00:36:52 +0200

    update

--- src/Components/Guess.js
+++ src/Components/Guess.js
@@ -4,53 +4,56 @@ const Guess = ({ word }) => {
   // rec pretvorena u donje crte
   const [hiddenWord, setHiddenWord] = useState([]);
   // pogresna slova koja su ukucana
-  const [guessedLetters, setGuessedLetters] = useState([]);
+  // const [guessedLetters, setGuessedLetters] = useState([]);
   // spojena rec posle za proveru
-  const joinedWord = word.join("");
+  // const joinedWord = word.join("");
 
   useEffect(() => {
+    const displayHiddenWord = () => {
+      const underscoreLetter = [];
+      for (const char of word) {
+        if (char === " ") {
+          underscoreLetter.push(" ");
+        } else {
+          underscoreLetter.push("_");
+        }
+      }
+      setHiddenWord(underscoreLetter);
+    };
+
     displayHiddenWord();
     // displayHiddenWord();
     // window.addEventListener("keydown", handleKeyPress);
     // setHiddenWord(underscoreLetter); //Maximum update depth exceeded. This can happen when a component calls setState inside useEffect, but useEffect either doesn't have a dependency array, or one of the dependencies changes on every render.
   }, [word]); // svaki put kad se promeni word updateuj
 
-  useEffect(() => {
-    const handleKeyPress = (e) => {
-      if (e.key.match(/[a-z]/i)) {
-        const letter = e.key.toUpperCase();
+  useEffect(() => {}, []);
 
-        checkAndReplace(letter, word);
-      }
-    };
-    window.addEventListener("keydown", handleKeyPress);
+  const handleKeyPress = (e) => {
+    if (e.key.match(/[a-z]/i)) {
+      const letter = e.key.toUpperCase();
 
-    const checkAndReplace = (letter, word) => {
-      if (word.includes(letter)) {
-        for (let i = 0; i <= word.length; i++) {
-          if (word[i] === letter) {
-            // hiddenWord[i] = letter;
-            const [...underscoreLetter] = hiddenWord;
-            underscoreLetter[i] = letter;
-            setHiddenWord(underscoreLetter);
-            console.log(hiddenWord);
-            // wordString.innerHTML = hiddenWord.join("");
-          }
+      checkAndReplace(letter);
+    }
+  };
+
+  window.addEventListener("keydown", handleKeyPress);
+
+  const checkAndReplace = (letter) => {
+    if (word.includes(letter)) {
+      for (let i = 0; i <= word.length; i++) {
+        if (word[i] === letter) {
+          // hiddenWord[i] = letter;
+          const [...underscoreLetter] = hiddenWord;
+          underscoreLetter[i] = letter;
+          setHiddenWord(underscoreLetter);
+          console.log(hiddenWord);
+          // wordString.innerHTML = hiddenWord.join("");
         }
       }
-    };
-  }, []);
-
-  const displayHiddenWord = () => {
-    const underscoreLetter = [];
-    for (const char of word) {
-      if (char === " ") {
-        underscoreLetter.push(" ");
-      } else {
-        underscoreLetter.push("_");
-      }
+    } else {
+      console.log("Ne include brt");
     }
-    setHiddenWord(underscoreLetter);
   };
 
   // proverava da li je kliknuto slovo i prosledjuje slovo funkciji koja se uporedjuje
